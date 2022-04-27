84d7feeb9051e35f9048c428b0e234c9fdba934d
Ben <ben.williams@noaa.gov>
2022-04-26 16:45:21 -0800

    add functions

--- R/utils.R
+++ R/utils.R
@@ -76,3 +76,21 @@ sql_run <- function(database, query) {
 collapse_filters <- function(x) {
   sprintf("'%s'", paste(x, collapse = "','"))
 }
+
+#' date of data query
+#'
+#' @param year assessment year
+#'
+#' @return
+#' @export
+#'
+#' @examples
+q_date <- function(year){
+  txt = "Data were downloaded on:"
+  dt = format(Sys.time(), "%Y-%m-%d")
+  
+  
+  write.table(c(txt, dt), file = here::here(year, "data", "raw", "data_called.txt"),
+              sep = "\t", col.names = F, row.names = F)
+}
+
