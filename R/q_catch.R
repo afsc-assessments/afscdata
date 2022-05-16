056ba7da48fce3edf2d2ac33a61acb2d25b03346
Ben <ben.williams@noaa.gov>
2022-05-16 13:36:23 -0800

    spelling...

--- R/q_catch.R
+++ R/q_catch.R
@@ -19,7 +19,7 @@ q_catch <- function(year, species, area, akfin, save = TRUE) {
   sql = sql_read("fsh_catch.sql")
   sql = sql_filter(sql_precode = "<=", year, sql_code = sql, flag = "-- insert year")
   sql = sql_filter(sql_precode = "IN", area, sql_code = sql, flag = "-- insert region")
-  sql = sql_filter(sql_precode = "IN", species, sql_code = sql, flag = "-- insert year")
+  sql = sql_filter(sql_precode = "IN", species, sql_code = sql, flag = "-- insert species")
   
   if(isTRUE(save)){
     sql_run(akfin, sql) %>%
