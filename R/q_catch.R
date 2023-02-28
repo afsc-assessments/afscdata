4d328d5723b2cd600b08f9d8d63fa9beef59af18
Ben <ben.williams@noaa.gov>
2023-02-27 15:23:32 -0900

    update doc and webpage

--- R/q_catch.R
+++ R/q_catch.R
@@ -1,16 +1,19 @@
 #' query fishery catch data
 #'
+#' Note that this function will produce results that are confidential. They can be hidden from git/GitHub by adding fsh_catch_data.csv to your .gitignore file
+#'
 #' @param year max year to retrieve data from 
 #' @param species species group code e.g., "DUSK"
 #' @param area sample area "GOA", "AI" or "BS" (or combos)
 #' @param db data server to connect to
 #' @param save saves a file to the data/raw folder, otherwise sends output to global enviro (default: TRUE)
 #'
-#' @description Note that this function will produce results that are confidential. They can be hidden from git/GitHub by adding fsh_catch_data.csv to your .gitignore file
+#' @return a .csv of catch data 
 #' @export
 #'
 #' @examples
 #' \dontrun{
+#' # query northern rockfish in the goa and save the results in default location
 #' q_catch(year = 2022, species = 'NORK', area = 'GOA', db = akfin, save = TRUE)
 #' }
 q_catch <- function(year, species, area, db, save = TRUE) {
