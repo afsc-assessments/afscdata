5f50b35730a97ad1dd376e81ecf887885d2dc3d4
Ben <ben.williams@noaa.gov>
2023-03-02 13:08:46 -0900

    cleanup to push to main

--- R/queries.R
+++ R/queries.R
@@ -3,40 +3,266 @@
 #' Note that this function will produce results that are confidential. They can be hidden from git/GitHub by adding fsh_catch_data.csv to your .gitignore file
 #'
 #' @param year max year to retrieve data from 
-#' @param species species group code e.g., "DUSK"
+#' @param species species group code e.g., "DUSK" or numeric agency values e.g. c("131", "132") - must be either all 4 digit or 3 digit codes
 #' @param area sample area "GOA", "AI" or "BS" (or combos)
-#' @param db data server to connect to
-#' @param use_afsc_code use 3 digit species code to filter instead of species group code (default: FALSE)
+#' @param db data server to connect to (akfin)
+#' @param area fmp_area (GOA, BSAI) or fmp_subarea (BS, AI, WG, CG, WY, EY, SE) - also available (SEI, PWSI)
+#' @param add_fields add other columns to the database (must currently exist on server). "*" will return all table columns available
+#' @param print_sql outputs the sql query instead of calling the data
 #' @param save saves a file to the data/raw folder, otherwise sends output to global enviro (default: TRUE)
+#' 
+q_catch <- function(year, species, area, db, add_fields=NULL, print_sql=FALSE, save=TRUE) {
+  
+  # globals 
+  area = toupper(area)
+  area = if(isTRUE(area == "GOA")){
+    area = c("WG", "CG", "WY", "EY", "SE")
+  } else if(isTRUE(area=="BSAI")){
+    area = c("BS", "AI")
+  } else {
+    area
+  }
+  
+  yr = year
+  
+  # select columns to import
+  if(add_fields == "*") {
+    table <- dplyr::tbl(db, sql("council.comprehensive_blend_ca")) %>% 
+      dplyr::rename_with(tolower) %>% 
+      dplyr::filter(year <= yr, fmp_subarea %in% area)
+  } else {
+    cols = c("year",                             
+             "agency_species_code",              
+             "species_group_name",               
+             "species_name",          
+             "species_group_code",               
+             "retained_or_discarded",            
+             "trip_target_code",                 
+             "trip_target_name",               
+             "cdq_flag",                  
+             "fmp_gear",                 
+             "agency_gear_code",                   
+             "fmp_area",                           
+             "fmp_subarea",                        
+             "reporting_area_code",   
+             "week_end_date",                      
+             "weight_posted",              
+             "vessel_id",                          
+             "ves_akr_length",                     
+             "sampling_strata",                    
+             "sampling_strata_name",               
+             "sampling_strata_deployment_category",
+             "sampling_strata_selection_rate",     
+             "deployment_trip_pk",                 
+             "deployment_trip_start_date",         
+             "deployment_trip_end_date",           
+             "adfg_stat_area_code",                
+             "akr_state_federal_waters_code",
+             tolower(add_fields))
+    
+  
+    table <- dplyr::tbl(db, sql("council.comprehensive_blend_ca")) %>% 
+      dplyr::rename_with(tolower) %>% 
+      dplyr::select(!!!cols) %>% 
+      dplyr::filter(year <= yr, fmp_subarea %in% area)
+  
+  }
+  
+  # filter species
+  if(isTRUE(sum(stringi::stri_length(species) - 3) == 0)){
+    dplyr::filter(table, agency_species_code %in% species) -> table
+  } else {
+    dplyr::filter(table, species_group_code %in% species) -> table
+  }
+  
+  # output
+  if(isTRUE(save)) {
+    if(isFALSE(dir.exists(here::here(year, "data")))) stop("you must run afscdata::setup_folders() before you can save to the default location")
+    dplyr::collect(table) %>% 
+      vroom::vroom_write(here::here(year, "data", "raw", "fsh_catch_data.csv"), 
+                         delim = ",")
+    
+    capture.output(show_query(table), 
+                   file = here::here(year, "data", "sql", "fsh_catch_sql.txt"))
+    
+    message("fishery catch data can be found in the data/raw folder")
+  } else if (isFALSE(save) & isFALSE(print_sql)) {
+    dplyr::collect(table)
+  } else {
+    dplyr::show_query(table)
+    message("this sql code is passed to the server")
+  }
+}
+  
+
+#' query foreign catch data from the AKFIN server
+#' 
+#' something about the species names and area codes?
+#' pulls data from akfin.pre1991.foreign_blend
 #'
-#' @return a fsh_catch_data.csv file to the data/raw folder
+#' @param year  max year to retrieve data from 
+#' @param species common name (e.g., "sablefish" or "all flounders") - can call multiple species
+#' @param area numeric area digit code
+#' @param db data server to connect to (akfin)
+#' @param print_sql outputs the sql query instead of calling the data (default: false)
+#' @param save saves a file to the data/raw folder, otherwise sends output to global enviro (default: true)
+#'
+#' @return
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' # query northern rockfish in the goa and save the results in default location
-#' q_catch(year = 2022, species = 'NORK', area = 'GOA', db = akfin, save = TRUE)
-#' }
-q_catch <- function(year, species, area, db, use_afsc_code = FALSE, save = TRUE) {
+q_for_catch <- function(year, species, area, db, print_sql=FALSE, save=TRUE){
+  yr = year
+  species=toupper(species)
   
+  tbl(db, sql("pre1991.foreign_blend")) %>% 
+    dplyr::rename_all(tolower) %>% 
+    dplyr::select(species_name, 
+                  tons = blend_tonnage,
+                  date = week_date,
+                  vessel_class,
+                  area_number,
+                  area_name) %>% 
+    dplyr::mutate(year = lubridate::year(date),
+                  month = lubridate::month(date),
+                  gear = dplyr::case_when(vessel_class %like% '%POT%' ~ "POT",
+                                          vessel_class %like% '%LONG%' ~ "HAL",
+                                          TRUE ~ "TRW")) %>% 
+    dplyr::group_by(species_name, year, month, area_number, gear, area_name) %>% 
+    dplyr::summarise(tons = sum(tons, na.rm = T)) %>% 
+    dplyr::filter(year<=yr, species_name %in% species, area_number %in% area) %>% 
+    dplyr::rename(species=species_name, area=area_number) %>% 
+    dplyr::arrange(species, year, month, area, gear) -> table
+  
+  if(isTRUE(save)) {
+    dplyr::collect(table) %>% 
+      vroom::vroom_write(here::here(year, "data", "raw", "for_catch_data.csv"), 
+                         delim = ",")
+    capture.output(show_query(table), 
+                   file = here::here(year, "data", "sql", "for_catch_sql.txt"))
+    
+    message("foreign catch data can be found in the data/raw folder")
+  } else if (isFALSE(save) & isFALSE(print_sql)) {
+    dplyr::collect(table)
+  } else {
+    dplyr::show_query(table)
+    message("this sql code is passed to the server")
+  }
+}
+
+#' query bottom trawl survey length data from the AFSC server
+#' 
+#'
+#' @param year  max year to retrieve data from 
+#' @param species 5 digit species code (e.g., 10110) - can place multiple in a vector c(10110, 10130)
+#' @param area bs, hwc, wc, hg, ai, goa, or hbs
+#' @param db data server to connect to (afsc)
+#' @param print_sql outputs the sql query instead of calling the data (default: false)
+#' @param save saves a file to the data/raw folder, otherwise sends output to global enviro (default: true)
+#' 
+q_bts_length <- function(year, species, area, db, print_sql=FALSE, save=TRUE){
+  
+  # globals
+  yr = year
   area = toupper(area)
   
-  # species_switch(species, area)
+  # pull data sources
+  dplyr::tbl(db, sql("racebase.cruise")) %>% 
+    rename_with(tolower) -> aa
   
-  catch = sql_read("fsh_catch.sql")
-  if(isTRUE(use_afsc_code)) {
-    stringr::str_replace(catch, "AND species_group_code", "AND agency_species_code")
-  }
-  catch = sql_filter(sql_precode = "<=", year, sql_code = catch, flag = "-- insert year")
-  catch = sql_filter(x = area, sql_code = catch, flag = "-- insert area")
-  catch = sql_filter(x = species, sql_code = catch, flag = "-- insert species")
+  dplyr::tbl(db, sql("racebase.haul")) %>% 
+    dplyr::rename_with(tolower) -> bb
   
-  if(isTRUE(save)){
-    sql_run(db, catch) %>%
-      vroom::vroom(here::here(year, "data", "raw", "fsh_catch_data.csv"), 
-                   delim = ",")
+  dplyr::tbl(db, sql("racebase.length")) %>% 
+    dplyr::rename_with(tolower) -> cc
+  # join, filter and query
+  dplyr::select(aa, cruisejoin, region, survey_name, start_date) %>% 
+    dplyr::left_join(dplyr::select(bb, cruisejoin, hauljoin, end_latitude, 
+                                   end_longitude, bottom_depth, abundance_haul, 
+                                   stratum)) %>%
+    dplyr::left_join(dplyr::select(cc, hauljoin, species_code, sex, length, 
+                                   frequency)) %>% 
+    dplyr::mutate(year = lubridate::year(start_date)) %>%
+    dplyr::select(-start_date) %>% 
+    dplyr::filter(abundance_haul == "Y", 
+                  year <= yr, 
+                  region %in% area, 
+                  species_code %in% species) %>% 
+    dplyr::arrange(year) -> table
+  
+  if(isTRUE(save)) {
+    dplyr::collect(table) %>% 
+      vroom::vroom_write(here::here(year, "data", "raw", "bts_length_data.csv"), 
+                         delim = ",")
+    capture.output(show_query(table), 
+                   file = here::here(year, "data", "sql", "bts_length_sql.txt"))
+    
+    message("bottom trawl survey length data can be found in the data/raw folder")
+  } else if (isFALSE(save) & isFALSE(print_sql)) {
+    dplyr::collect(table)
   } else {
-    sql_run(db, catch)
+    dplyr::show_query(table)
+    message("this sql code is passed to the server")
   }
+  
+}
+
+
+#' query bottom trawl survey specimen data from the AFSC server
+#' 
+#'
+#' @param year  max year to retrieve data from 
+#' @param species 5 digit species code (e.g., 10110) - can place multiple in a vector c(10110, 10130)
+#' @param area bs, hwc, wc, hg, ai, goa, or hbs
+#' @param db data server to connect to (afsc)
+#' @param print_sql outputs the sql query instead of calling the data (default: false)
+#' @param save saves a file to the data/raw folder, otherwise sends output to global enviro (default: true)
+#' 
+q_bts_specimen <- function(year, species, area, db, print_sql=FALSE, save=TRUE){
+  
+  # globals
+  yr = year
+  area = toupper(area)
+  
+  # pull data sources
+  dplyr::tbl(db, sql("racebase.cruise")) %>% 
+    rename_with(tolower) -> aa
+  
+  dplyr::tbl(db, sql("racebase.haul")) %>% 
+    dplyr::rename_with(tolower) -> bb
+  
+  dplyr::tbl(db, sql("racebase.specimen")) %>% 
+    dplyr::rename_with(tolower) -> cc
+  
+  # join, filter and query
+  dplyr::select(aa, cruisejoin, region, survey_name, start_date) %>% 
+    dplyr::left_join(dplyr::select(bb, cruisejoin, hauljoin, end_latitude, 
+                                   end_longitude, bottom_depth, abundance_haul, 
+                                   stratum)) %>%
+    dplyr::left_join(dplyr::select(cc, hauljoin, species_code, sex, 
+                                   length, weight, age, maturity)) %>% 
+    dplyr::mutate(year = lubridate::year(start_date)) %>%
+    dplyr::select(-start_date) %>% 
+    dplyr::filter(abundance_haul == "Y", 
+                  year <= yr, 
+                  region %in% area, 
+                  species_code %in% species) %>% 
+    dplyr::arrange(year) -> table
+  
+  if(isTRUE(save)) {
+    dplyr::collect(table) %>% 
+      vroom::vroom_write(here::here(year, "data", "raw", "bts_specimen_data.csv"), 
+                         delim = ",")
+    capture.output(show_query(table), 
+                   file = here::here(year, "data", "sql", "bts_specimen_sql.txt"))
+    
+    message("bottom trawl survey specimen data can be found in the data/raw folder")
+  } else if (isFALSE(save) & isFALSE(print_sql)) {
+    dplyr::collect(table)
+  } else {
+    dplyr::show_query(table)
+    message("this sql code is passed to the server")
+  }
+  
   
 }
