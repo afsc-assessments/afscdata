63e00c766e05487088aa8d98d95be9281372c5b2
Ben <ben.williams@noaa.gov>
2023-03-03 13:09:39 -0900

    ll query

--- R/queries.R
+++ R/queries.R
@@ -1,3 +1,157 @@
+#' query nfms longline survey relative population numbers or weights
+#' 
+#' available source tables on akfin:
+#' lls_area_stratum_rpn
+#' lls_area_stratum_rpn_depred
+#' lls_area_rpn_all_strata
+#' lls_area_rpn_3_to_7
+#' lls_area_rpn_3_to_7_depred
+#' lls_council_sablefish_area_all_strata
+#' lls_council_sablefish_area_3_to_7_depred
+#' lls_fmp_subarea_all_strata
+#' lls_fmp_subarea_3_to_7_depred
+#' lls_fmp_all_strata* not available in this fcn
+#' lls_ak_wide_3_to_7_depred
+#'  
+#' @param year max year to retrieve data from 
+#' @param area options are 'goa', 'bs', 'ai', or a combo. default=c('goa', 'bs', 'ai')
+#' @param species 5 digit afsc/race species code(s) e.g., 79210 or c(79210, 10110)
+#' @param by 'depth' (stratum-level), 'geoarea' (e.g. Spencer Gully, Kodiak
+#'   slope), 'councilarea' (e.g., West Yakutat, East Yakutat/Southeast),
+#'   'fmpsubarea' (e.g., Eastern Gulf of Alaska), or 'akwide' (only for
+#'   sablefish). default: 'fmpsubarea' - can only call a single area
+#' @param use_historical T/F include historical Japanese survey data in the
+#'   results (default: false)
+#' @param db  the database to query (akfin)
+#' @param print_sql outputs the sql query instead of calling the data (default: false) - save must be false
+#' @param save save the file in designated folder, if FALSE outputs to global environment
+#' 
+q_lls_rpn <- function(year, area=c('goa', 'bs', 'ai'), species, by='fmpsubarea', 
+                      use_historical=FALSE, db, print_sql=FALSE, save=TRUE) {
+  
+  # adjust filters
+  yr = year
+  sp = species
+  if(by %in% c('akwide', 'AKWIDE')) {
+    message("when by='akwide', the area argument is ignored within this function.\n")
+    area <- c('goa', 'bs', 'ai')
+  }
+  if(any(area == 'bsai')) {
+    area <- c(area[area != 'bsai'], 'bs', 'ai')
+  }
+  area = toupper(area)
+  by = tolower(by)
+  
+  # message center
+  if(any(!area %in% c('GOA', 'BS', 'AI'))) {
+    stop("the appropriate args for area are 'goa', 'bs', 'ai', or a combo. defaults to c('goa', 'bs', 'ai')")
+  }
+  
+  if(any(!by %in% c('depth', 'geoarea', 'councilarea', 'fmpsubarea', 'akwide')) | length(by) > 1){ 
+    stop("appropriate args for by are: 'depth', 'geoarea', 'councilarea', 'fmpsubarea',  or 'akwide'.\n 
+         only a single arg may be used.")
+  }
+  
+  if(is.logical(use_historical)==FALSE) {
+    stop('appropriate args for use_historical are TRUE or FALSE')
+  } else {
+    if(isFALSE(use_historical)) {
+      srv <- 'United States'
+    } else {
+      srv <- c('United States', 'Japan')
+    } 
+  }
+  # special sablefish
+  if(species == 20510) {
+    strata <- '_3_to_7'
+    depred <- '_depred'
+    message("sablefish rpns:\n
+            -are corrected for sperm whale depredation and area summaries exlude gully stations (stratum 2b) \n
+            -use interpolated bs/ai values in alternating survey years when aggregated to the fmp or ak-wide level \n
+            -assume fixed rpn/rpw data in the ai (1990-1995) and bs (1990-1996) when no bs/ai surveys occurred\n
+            -assume fixed ak-wide rpn/rpws from 1979-1994 for the historical Japanese survey \n")
+  } else {
+    strata <- '_all_strata'
+    depred <- ''
+  }
+  
+  # decide which tables to use 
+  if(by == 'depth') {
+    table = dplyr::tbl(db, dplyr::sql(paste0("afsc.lls_area_stratum_rpn", depred))) %>%
+      # Code used to flag geographic areas for calculations of relative
+      # population weights and numbers.
+      dplyr::filter(EXPLOITABLE == '1')
+  } else if(by == 'geoarea') {
+    table = dplyr::tbl(db, dplyr::sql(paste0("afsc.lls_area_rpn", strata, depred))) %>%
+      dplyr::filter(EXPLOITABLE == '1')
+  } else if(by == 'councilarea') {
+    table = dplyr::tbl(db, dplyr::sql(paste0("afsc.lls_council_sablefish_area", strata, depred)))
+  } else if(by == 'fmpsubarea') {
+    table = dplyr::tbl(db, dplyr::sql(paste0("afsc.lls_fmp_subarea", strata, depred)))
+  } else if(by == 'akwide') {
+    if(species != 20510) {
+      stop("ak-wide summaries are only available for sablefish. the lls alternates between the bs/ai in odd/even years, and interpolation methods should be evaluated on a species-specific basis.")
+    }
+    table = dplyr::tbl(db, dplyr::sql(paste0("afsc.lls_ak_wide", strata, depred)))
+  }
+  
+  # set up filters that are consistent across all tables
+  table <- table %>% 
+    dplyr::rename_with(tolower) %>% 
+    dplyr::filter(year <= yr, 
+                  species_code %in% sp,
+                  country %in% srv)
+  
+  # set up table-specific area filters
+  area_lkup = data.frame(fmparea = c('BS', 'AI', 'GOA', 'GOA', 'GOA', 'GOA'),
+                         councilarea = c('Bering Sea', 'Aleutians', 
+                                         'East Yakutat/Southeast', 
+                                         'West Yakutat', 
+                                         'Central Gulf of Alaska', 
+                                         'Western Gulf of Alaska'),
+                         fmpsubarea = c('Bering Sea', 'Aleutians', 
+                                        'Eastern Gulf of Alaska', 
+                                        'Eastern Gulf of Alaska', 
+                                        'Central Gulf of Alaska', 
+                                        'Western Gulf of Alaska')) %>% 
+    dplyr::filter(fmparea %in% area)
+  
+  if(by %in% c('depth', 'geoarea', 'councilarea')) {
+    tmparea <- base::unique(area_lkup$councilarea)
+    table <- table %>% dplyr::filter(council_sablefish_management_area %in% tmparea) 
+  }
+  if(by %in% c('fmpsubarea')) {
+    tmparea <- base::unique(area_lkup$fmpsubarea)
+    table <- table %>% dplyr::filter(council_management_area %in% tmparea) 
+  }
+  
+  # id for saving data and sql files
+  id = by
+  if(by == 'depth') {
+    id = "depthstratum"
+  }
+  
+  if(isTRUE(save)){
+    if(isFALSE(dir.exists(here::here(year, "data")))) {
+      stop("you must run afscdata::setup_folders() before you can save to the default location")
+      }
+    dplyr::collect(table) %>% 
+      vroom::vroom_write(here::here(year, "data", "raw", paste0("lls_rpn_", id, "_data.csv")), 
+                         delim = ",")
+    capture.output(show_query(table), 
+                   file = here::here(year, "data", "sql", paste0("lls_rpn_", id, "sql.txt")))
+    
+    message("longline survey data can be found in the data/raw folder,\n
+            and the associated query is saved in data/sql")
+  } else if (isFALSE(save) & isFALSE(print_sql)) {
+    dplyr::collect(table)
+  } else {
+    dplyr::show_query(table)
+    message("this sql code is passed to the server")
+  }
+  
+}
+
 #' query fishery catch data from AKFIN server
 #'
 #' Note that this function produces results that may be confidential. 
@@ -8,7 +162,7 @@
 #' @param area fmp_area (GOA, BSAI) or fmp_subarea (BS, AI, WG, CG, WY, EY, SE) - also available (SEI, PWSI), can use all fmp_areas or all fmp_subareas, but don't mix the two
 #' @param db data server to connect to (akfin)
 #' @param add_fields add other columns to the database (must currently exist on server). "*" will return all table columns available
-#' @param print_sql outputs the sql query instead of calling the data
+#' @param print_sql outputs the sql query instead of calling the data - save must be false
 #' @param save saves a file to the data/raw folder, otherwise sends output to global enviro (default: TRUE)
 #' 
 #' @return saves catch data as data/raw/fsh_catch_data.csv or outputs to the global environment, save also saves a copy of the SQL code used for the query and stores it in the data/sql folder.
@@ -118,7 +272,7 @@ q_catch <- function(year, species, area, db, add_fields=NULL, print_sql=FALSE, s
 #' @param species common name (e.g., "sablefish" or "all flounders") - can call multiple species
 #' @param area numeric area digit code - multiples is ok
 #' @param db data server to connect to (akfin)
-#' @param print_sql outputs the sql query instead of calling the data (default: false)
+#' @param print_sql outputs the sql query instead of calling the data (default: false) - save must be false
 #' @param save saves a file to the data/raw folder, otherwise sends output to global enviro (default: true)
 #'
 #' @return saves catch data as data/raw/for_catch_data.csv or outputs to the global environment, also saves a copy of the SQL code used for the query and stores it in the data/sql folder. 
@@ -177,7 +331,7 @@ q_for_catch <- function(year, species, area, db, print_sql=FALSE, save=TRUE){
 #' @param species 5 digit species code (e.g., 10110) - can place multiple in a vector c(10110, 10130)
 #' @param area bs, ai, goa, or hwc, wc, hg, hbs - can do multiples c("bs","ai")
 #' @param db data server to connect to (afsc)
-#' @param print_sql outputs the sql query instead of calling the data (default: false)
+#' @param print_sql outputs the sql query instead of calling the data (default: false) - save must be false
 #' @param save saves a file to the data/raw folder, otherwise sends output to global enviro (default: true)
 #' 
 #' @return saves bts length data as data/raw/bts_length_data.csv or outputs to the global environment, also saves a copy of the SQL code used for the query and stores it in the data/sql folder. 
@@ -243,7 +397,7 @@ q_bts_length <- function(year, species, area, db, print_sql=FALSE, save=TRUE){
 #' @param species 5 digit species code (e.g., 10110) - can place multiple in a vector c(10110, 10130)
 #' @param area bs, ai, goa, or hwc, wc, hg, hbs - can do multiples c("bs","ai")
 #' @param db data server to connect to (afsc)
-#' @param print_sql outputs the sql query instead of calling the data (default: false)
+#' @param print_sql outputs the sql query instead of calling the data (default: false) - save must be false
 #' @param save saves a file to the data/raw folder, otherwise sends output to global enviro (default: true)
 #' 
 #' @return saves bts length data as data/raw/bts_length_data.csv or outputs to the global environment, also saves a copy of the SQL code used for the query and stores it in the data/sql folder. 
