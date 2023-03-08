428333e53212dce5d857e19a095b4750919655e6
Ben <ben.williams@noaa.gov>
2023-03-08 14:51:34 -0900

    annotate

--- R/goa_rockfish.R
+++ R/goa_rockfish.R
@@ -37,8 +37,10 @@ goa_nork <- function(year, off_yr = FALSE){
     disconnect(afsc)
   }
   
+  # read in archived catch data
   afscdata::goa_nork_catch_1961_1992 %>%
     vroom::vroom_write(here::here(year, "data", "user_input", "goa_nork_catch_1961_1992.csv"))
   
+  # timestamp
   q_date(year)
 }
