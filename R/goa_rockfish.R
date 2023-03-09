129c615fda717561d6fe8ca0c719fcb91a81af88
Ben <ben.williams@noaa.gov>
2023-03-08 15:33:13 -0900

    update rockfish

--- R/goa_rockfish.R
+++ R/goa_rockfish.R
@@ -32,8 +32,8 @@ goa_nork <- function(year, off_yr = FALSE){
     disconnect(akfin)  
     
     afsc = connect("afsc")
-    q_bts_specimen(year, species=afsc_species, area, db, print_sql=FALSE, save=TRUE)
-    q_bts_length(year, species=afsc_species, area, db, print_sql=FALSE, save=TRUE)  
+    q_bts_specimen(year, species=afsc_species, area, db)
+    q_bts_length(year, species=afsc_species, area, db)  
     disconnect(afsc)
   }
   
@@ -42,5 +42,52 @@ goa_nork <- function(year, off_yr = FALSE){
     vroom::vroom_write(here::here(year, "data", "user_input", "goa_nork_catch_1961_1992.csv"))
   
   # timestamp
-  q_date(year)
+  afscdata:::q_date(year)
+}
+
+#' raw data query for GOA dusky rockfish
+#'
+#' @param year assessment year
+#' @param off_yr if this is an off-year assessment change to TRUE
+#'
+#' @return a suite of raw data .csv files and a time stamp of when the query was done 
+#' @export goa_dusk
+#'
+#' @examples
+#' \dontrun{
+#' goa_dusk(year = 2022, off_yr = FALSE)
+#'}
+goa_dusk <- function(year, off_yr = FALSE){
+  # globals 
+  area = "GOA"
+  species = c("PEL7", "PELS", "DUSK")
+  afsc_species = c(30150, 30152)
+  norpac_species = 330
+  
+  akfin = connect()
+  
+  q_catch(year=year, species=species, area=area, db=akfin)
+  # commented functions are currently in development
+  # q_obs(year=year, species=norpac_species, area=area, db=akfin)
+  q_bts_biomass(year=year, area=area, species=afsc_species, by='total', db=akfin) 
+  
+  if(isTRUE(off_yr)) {
+    disconnect(akfin) 
+  } else {
+    # q_fsh_specimen(year=year, species=afsc_species, area=area, db=akfin)
+    # q_fsh_length(year=year, species=afsc_species, area=area, db=akfin)  
+    disconnect(akfin)  
+    
+    afsc = connect("afsc")
+    q_bts_specimen(year=year, species=afsc_species, area=area, db=afsc)
+    q_bts_length(year=year, species=afsc_species, area=area, db=afsc)  
+    disconnect(afsc)
+  }
+  
+  # read in archived catch data
+  afscdata::goa_dusk_catch_1977_1990 %>%
+    vroom::vroom_write(here::here(year, "data", "user_input", "goa_dusk_catch_1977_1990.csv"))
+  
+  # timestamp
+  afscdata:::q_date(year)
 }
