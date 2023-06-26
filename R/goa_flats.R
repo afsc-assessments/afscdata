155c8a5e58a04250300c0a97cddbedc6c3b32cdb
Ben <ben.williams@noaa.gov>
2023-06-26 07:40:26 -0800

    flat update

--- R/goa_flats.R
+++ R/goa_flats.R
@@ -26,8 +26,8 @@ goa_fhs <- function(year, off_yr = FALSE){
   if(isTRUE(off_yr)) {
     disconnect(akfin) 
   } else {
-    q_fsh_specimen(year, species, area, db, print_sql=FALSE, save=TRUE)
-    q_fsh_length(year, species, area, db, print_sql=FALSE, save=TRUE)  
+    q_fsh_specimen(year, species, area, db=akfin, print_sql=FALSE, save=TRUE)
+    q_fsh_length(year, species, area, db=akfin, print_sql=FALSE, save=TRUE)  
     disconnect(akfin)  
     
     afsc = connect("afsc")
