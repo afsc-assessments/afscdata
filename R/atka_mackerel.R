8d3c3656be35dbb9ff434b8be77ae0ff6ac3249a
Ben <ben.williams@noaa.gov>
2023-09-27 13:54:20 -0800

    add goa_amak

--- R/atka_mackerel.R
+++ R/atka_mackerel.R
@@ -34,6 +34,8 @@ bsai_amak <- function(year, off_yr = FALSE){
     afsc = connect("afsc")
     q_bts_specimen(year=year, species=afsc_species, area=area, db=afsc)
     q_bts_length(year=year, species=afsc_species, area=area, db=afsc)  
+    q_bts_agecomp(year = year, species = afsc_species, area = area, db = afsc)
+    q_bts_sizecomp(year = year, species = afsc_species, area = area, db = afsc)
     disconnect(afsc)
   }
   
@@ -41,6 +43,32 @@ bsai_amak <- function(year, off_yr = FALSE){
   afscdata::bsai_amak_catch_1977_1990 %>%
     vroom::vroom_write(here::here(year, "data", "user_input", "bsai_amak_catch_1977_1990.csv"), ",")
   
+  # timestamp
+  q_date(year)
+}
+
+#' raw data query for the GOA Atka mackerel assessment
+#'
+#' @param year assessment year
+#'
+#' @return a suite of raw data .csv files and a time stamp of when the query was done 
+#' @export goa_amak
+#'
+#' @examples
+#' \dontrun{
+#goa_amak(year = 2023)
+#'}
+goa_amak <- function(year) {
+  # globals 
+  area = "GOA"
+  species = "AMCK"
+  afsc_species = 21921
+  norpac_species = 193
+  
+  akfin = afscdata::connect()
+  q_catch(year=year, species=species, area=area, db=akfin)
+  q_bts_biomass(year, area=area, species=afsc_species, type='total', db=akfin) 
+  disconnect(akfin)
   # timestamp
   q_date(year)
 }
