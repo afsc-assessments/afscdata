df4aed581317873434af0f92e6d6c8cd7e65e2dd
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2023-12-18 14:04:52 -0800

    fill in codes
    still need to update norpac; connect() is giving issues

--- R/bsai_flats.R
+++ R/bsai_flats.R
@@ -54,4 +54,45 @@ bsai_atf <- function(year, off_yr = FALSE){
 }
 
 bsai_fhs<- function(year, off_yr = FALSE){
-}
+  # globals 
+  area = "BSAI"
+  species = "FSOL" 
+  afsc_species = c(103,145) ## fhs, bering flounder
+  norpac_species = 141
+  
+  akfin = connect()
+  
+  q_catch(year, species=species, area=area, db=akfin)
+  q_fish_obs(year, species=norpac_species, area=area, db=akfin)
+  q_bts_biomass(year, area="BS", species=afsc_species, db=akfin, save=F) %>% 
+    dplyr::mutate(species_code = as.numeric(species_code)) %>% 
+    dplyr::select(survey, year, species_code, haul_count=haulcount, catch_count=catcount, 
+                  mean_wgt_cpue = meanwgtcpue, var_wgt_cpue = varmnwgtcpue, 
+                  mean_num_cpue = meannumcpue, var_num_cpue = varmnnumcpue, 
+                  total_biomass = biomass, biomass_var=varbio, 
+                  min_biomass=lowerb, max_biomass = upperb, total_pop = population,
+                  pop_var = varpop, min_pop = lowerp, max_pop = upperp, akfin_load_date) %>% 
+    dplyr::bind_rows(q_bts_biomass(year, area="AI", species=afsc_species, db=akfin, save=F)) %>% 
+    vroom::vroom_write(here::here(year, "data", "raw", "bts_biomass_data.csv"), delim = ",")
+  
+  if(isTRUE(off_yr)) {
+    disconnect(akfin) 
+  } else {
+    q_fsh_specimen(year, species, area, db=akfin, print_sql=FALSE, save=TRUE)
+    q_fsh_length(year, species, area, db=akfin, print_sql=FALSE, save=TRUE)  
+    disconnect(akfin)  
+    
+    afsc = connect("afsc")
+    q_bts_specimen(year, species=afsc_species, area, db=afsc)
+    q_bts_length(year, species=afsc_species, area, db=afsc)  
+    disconnect(afsc)
+  }
+  
+  # read in archived catch data
+  # afscdata::goa_atf_catch_1961_1990 %>%
+  # vroom::vroom_write(here::here(year, "data", "user_input", "goa_atf_catch_1961_1990.csv"), ",")
+  
+  # timestamp
+  q_date(year)
+  
+  }
