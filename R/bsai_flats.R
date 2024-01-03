da016ec0924dd3475edffe18f6ead0518acbe98b
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-01-03 12:32:53 -0800

    length data already is sex specific

--- R/bsai_flats.R
+++ R/bsai_flats.R
@@ -156,7 +156,7 @@ bsai_fhs<- function(year, off_yr = FALSE){
     disconnect(akfin) 
   } else {
     q_fsh_specimen(year, species=norpac_species, area, db=akfin, print_sql=FALSE, save=TRUE, add_fields='sex')
-    q_fsh_length(year, species=norpac_species, area, db=akfin, print_sql=FALSE, save=TRUE, add_fields='sex') 
+    q_fsh_length(year, species=norpac_species, area, db=akfin, print_sql=FALSE, save=TRUE) 
     disconnect(akfin)  
     
     afsc = connect("afsc")
