96ed9cea60f38f65a27553a2b14483e2c6ce3180
Ben Williams <ben.williams@noaa.gov>
2026-04-22 14:32:07 -0800

    norpac

--- R/goa_rockfish.R
+++ R/goa_rockfish.R
@@ -30,8 +30,8 @@ goa_nork <- function(year, off_yr = FALSE){
     q_bts_gap_length(year, species=afsc_species, area, db=akfin)  
     q_bts_gap_agecomp(year = year, species = afsc_species, area = area, db = akfin)
     q_bts_gap_sizecomp(year = year, species = afsc_species, area = area, db = akfin)
-    q_fsh_specimen(year = year, species = afsc_species, area = area, db = akfin)
-    q_fsh_length(year = year, species = afsc_species, area = area, db = akfin)
+    q_fsh_specimen(year = year, species = norpac_species, area = area, db = akfin)
+    q_fsh_length(year = year, species = norpac_species, area = area, db = akfin)
     
     disconnect(akfin)
   }
@@ -76,8 +76,8 @@ goa_dusk <- function(year, off_yr = FALSE){
     q_bts_gap_length(year=year, species=afsc_species, area=area, db=akfin)  
     q_bts_gap_agecomp(year = year, species = afsc_species, area = area, db = akfin)
     q_bts_gap_sizecomp(year = year, species = afsc_species, area = area, db = akfin)
-    q_fsh_specimen(year = year, species = afsc_species, area = area, db = akfin)
-    q_fsh_length(year = year, species = afsc_species, area = area, db = akfin)
+    q_fsh_specimen(year = year, species = norpac_species, area = area, db = akfin)
+    q_fsh_length(year = year, species = norpac_species, area = area, db = akfin)
     disconnect(akfin)
   }
   
@@ -125,8 +125,8 @@ goa_pop <- function(year, off_yr = FALSE){
     q_bts_gap_length(year = year, species = afsc_species, area = area, db = akfin)  
     q_bts_gap_agecomp(year = year, species = afsc_species, area = area, db = akfin)
     q_bts_gap_sizecomp(year = year, species = afsc_species, area = area, db = akfin)
-    q_fsh_specimen(year = year, species = afsc_species, area = area, db = akfin)
-    q_fsh_length(year = year, species = afsc_species, area = area, db = akfin)
+    q_fsh_specimen(year = year, species = norpac_species, area = area, db = akfin)
+    q_fsh_length(year = year, species = norpac_species, area = area, db = akfin)
     disconnect(akfin)
   }
   
@@ -180,8 +180,8 @@ goa_rebs <- function(year, off_yr = FALSE){
     q_bts_gap_length(year=year, species=afsc_species, area=area, db=akfin)  
     q_bts_gap_agecomp(year = year, species = afsc_species, area = area, db = akfin)
     q_bts_gap_sizecomp(year = year, species = afsc_species, area = area, db = akfin)
-    q_fsh_specimen(year = year, species = afsc_species, area = area, db = akfin)
-    q_fsh_length(year = year, species = afsc_species, area = area, db = akfin)
+    q_fsh_specimen(year = year, species = norpac_species, area = area, db = akfin)
+    q_fsh_length(year = year, species = norpac_species, area = area, db = akfin)
     disconnect(akfin)
   }
   
@@ -231,8 +231,8 @@ goa_thornyhead <- function(year, off_yr = FALSE, catch_report = FALSE){
     q_bts_gap_length(year, species=afsc_species, area, db=akfin)  
     q_bts_gap_agecomp(year = year, species = afsc_species, area = area, db = akfin)
     q_bts_gap_sizecomp(year = year, species = afsc_species, area = area, db = akfin)
-    q_fsh_specimen(year = year, species = afsc_species, area = area, db = akfin)
-    q_fsh_length(year = year, species = afsc_species, area = area, db = akfin)
+    q_fsh_specimen(year = year, species = norpac_species, area = area, db = akfin)
+    q_fsh_length(year = year, species = norpac_species, area = area, db = akfin)
     disconnect(akfin) 
   }
   
